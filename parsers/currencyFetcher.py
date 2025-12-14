1c6ff707eff16324416a642f78a4f50eb724bd44
Dmitrii Barannikov <nensaty@gmail.com>
2025-12-14 15:33:16 +0500

    currencyFetcher updated

--- parsers/currencyFetcher.py
+++ parsers/currencyFetcher.py
@@ -2,7 +2,24 @@
 Fetches currency from Germany citizens.
 '''
 
+from typing import List
+import requests
 from bs4 import BeautifulSoup
 
-print('Dummy log from currencyFetcher')
+print('currencyFetcher initialized...')
 
+def get_html(url: str) -> str: #Getting url HTML raw txt
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            print(f'Successfully connected to {url}')
+            return response.text
+        else:   
+            print(f'Failed to get {url}')
+            return None
+    except Exception as e:
+        print('Error occured while trying to get HTML from {url}: {e.message}')
+        return None
+
+def get_data(html: str) -> List[dict]:
+    pass
