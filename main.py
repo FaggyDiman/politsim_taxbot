2c7630a0f9c0a2d5224db457a5f0761d5d27fd76
Dmitrii Barannikov <nensaty@gmail.com>
2025-12-14 20:07:30 +0500

    test

--- main.py
+++ main.py
@@ -2,17 +2,29 @@
 Entry point for the workflow (master module)
 '''
 
-import parsers.dbFetcher
-import parsers.currencyFetcher as getWealth
+import os
+from parsers import dbFetcher 
+from parsers import currencyFetcher
 import publisher.apiPublisher
 
+fancy_text = 'Execution stopped'
 
-html = getWealth.get_html('https://politsim.ru/semenar_update_currency_rates_test.php')
-if html is not None:
-    data = getWealth.get_data(html)
-    if data is not None:
-        pass
-    else:
-        raise RuntimeError('THE END...')
-else:
-    raise RuntimeError('THE END...')
+credentials = {
+    'host': os.getenv('DB_HOST'),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PSWD'),
+    'database':os.getenv('DB_NAME')
+    }
+
+if __name__ == "__main__":
+    html = currencyFetcher.get_html('https://politsim.ru/semenar_update_currency_rates_test.php')
+    if html is None:
+        raise RuntimeError(fancy_text)
+    
+    data = currencyFetcher.get_data(html)
+    if data is None:
+        raise RuntimeError(fancy_text)
+    
+    database = dbFetcher.connect_to_db(*credentials)
+    if database is None:
+        raise RuntimeError(fancy_text)
--- parsers/dbFetcher.py
+++ parsers/dbFetcher.py
@@ -1,11 +1,19 @@
 '''
-Fetches shop items from XF database
+Fetches value of inventories of players in XF database.
 '''
 
-import os
+import pymysql
 
-ab = os.getenv('DB_HOST')
-if ab is not None:
-    print('secret succesful')
-else:
-    print('Damn it')
+def connect_to_db(host: str, user: str, password: str, database: str) -> pymysql.connect:
+    connection = pymysql.connect(host=host,
+                             user=user,
+                             password=password,
+                             database=database,
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+    if connection.open:
+        print("Connection to the database established")
+        return connection
+    else:
+        print("Couldn't connect to the database")
+        return 0
